72a9d4692fd7cdbdd8ba35c871ca9a27d4673871
sproj <jonesalan404@gmail.com>
2024-12-10 21:59:18 +0000

    begin CI setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", hello_world());
+}
+
+fn hello_world() -> &'static str {
+    "Hello, world!"
+}
+
+#[cfg(test)]
+mod tests {
+    use super::hello_world;
+
+    #[test]
+    fn should_return_hello() {
+        assert_eq!(hello_world(), "Hello, world!");
+    }
 }
