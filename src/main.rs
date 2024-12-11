1128391734bfbeb124fec823c695c008e3a0a92c
sproj <jonesalan404@gmail.com>
2024-12-11 16:57:28 +0000

    bootstrap actix_web add health_check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,32 @@
-fn main() {
-    println!("{}", hello_world());
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", name)
 }
 
-fn hello_world() -> &'static str {
-    "Hello, world!"
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().finish()
 }
 
-#[cfg(test)]
-mod tests {
-    use super::hello_world;
-
-    #[test]
-    fn should_return_hello() {
-        assert_eq!(hello_world(), "Hello, world!");
-    }
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::hello_world;
+//     #[test]
+//     fn should_return_hello() {
+//         assert_eq!(hello_world(), "Hello, world!");
+//     }
+// }
