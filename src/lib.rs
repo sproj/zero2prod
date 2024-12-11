df4459a0cf6b6644b360d13f83909da38c8bc346
sproj <jonesalan404@gmail.com>
2024-12-11 23:21:29 +0000

    health_check black box test

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,13 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::net::TcpListener;
 
-pub async fn run() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
-}
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .listen(listener)?
+        .run();
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", name)
+    Ok(server)
 }
 
 async fn health_check() -> impl Responder {
