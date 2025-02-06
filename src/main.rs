9afa71b7d733fb785eb98f8ec4ea50b6b8c3c264
sproj <jonesalan404@gmail.com>
2025-02-06 17:06:22 +0000

    add db integration to subscriptions with ropey new db per test run

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::net::TcpListener;
+use sqlx::{PgPool};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
@@ -6,8 +7,13 @@ use zero2prod::startup::run;
 async fn main() -> Result<(), std::io::Error> {
     // Panic if configuration unreadable
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(
+        &configuration.database.connection_string()
+    )
+        .await
+    .expect("Failed to connect to Postgres.");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
