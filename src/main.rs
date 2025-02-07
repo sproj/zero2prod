838e81d8be85fcb4c5db6c47ed88536694230f21
sproj <jonesalan404@gmail.com>
2025-02-07 11:52:24 +0000

    add env_logger

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
@@ -5,6 +6,11 @@ use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    // `init` does call `set_logger`, so this is all we need to do.
+    // We are falling back to printing all logs at info-level or above
+    // if the RUST_LOG environment variable has not been set.
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     // Panic if configuration unreadable
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
@@ -12,6 +18,7 @@ async fn main() -> Result<(), std::io::Error> {
         .expect("Failed to connect to Postgres.");
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
+
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
 }
