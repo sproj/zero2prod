336f08b62d683feaeeb52cf03727675d640e4085
sproj <jonesalan404@gmail.com>
2025-05-29 21:24:01 +0100

    sqlx -> tokio-postgres + deadpool-postgres

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,5 +1,7 @@
 use actix_web::{web, HttpResponse};
-use sqlx::PgPool;
+use chrono::Utc;
+use deadpool_postgres::Pool;
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -15,7 +17,7 @@ pub struct FormData {
         subscriber_name = %form.name
     )
 )]
-pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<Pool>) -> HttpResponse {
     match insert_subscriber(&pool, &form).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(e) => {
@@ -29,25 +31,26 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     name = "Saving new subscriber details in the database",
     skip(form, pool)
 )]
-pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
-    sqlx::query!(
-        r#"
+pub async fn insert_subscriber(
+    pool: &Pool,
+    form: &FormData,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let client = pool.get().await?;
+    client
+        .execute(
+            r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
     VALUES ($1, $2, $3, $4)
     "#,
-        uuid::Uuid::new_v4(),
-        form.email,
-        form.name,
-        chrono::Utc::now()
-    )
-    .execute(pool)
-    .await
-    .map_err(|e| {
-        tracing::error!("Failed to execute query: {:?}", e);
-        e
-        // Using the `?` operator to return early
-        // if the function failed, returning a sqlx::Error
-        // We will talk about error handling in depth later!
-    })?;
+            &[&Uuid::new_v4(), &form.email, &form.name, &Utc::now()],
+        )
+        .await
+        .map_err(|e| {
+            tracing::error!("Failed to execute query: {:?}", e);
+            e
+            // Using the `?` operator to return early
+            // if the function failed, returning a sqlx::Error
+            // We will talk about error handling in depth later!
+        })?;
     Ok(())
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,11 +1,11 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
-use sqlx::PgPool;
+use deadpool_postgres::Pool;
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
 
-pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener, db_pool: Pool) -> Result<Server, std::io::Error> {
     let db_pool = web::Data::new(db_pool);
 
     let server = HttpServer::new(move || {
