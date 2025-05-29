9792fcef69ff514adb11bdedaf24c2b9f87e55bb
sproj <jonesalan404@gmail.com>
2025-05-29 21:24:01 +0100

    sqlx -> tokio-postgres + deadpool-postgres

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
+use deadpool_postgres::{Config, Pool, Runtime};
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use tokio_postgres::NoTls;
 use std::net::TcpListener;
-use zero2prod::configuration::get_configuration;
+use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
     // Panic if configuration unreadable
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool =
-        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
-            .expect("Failed to create Postgres connection pool.");
+    // create database connection pool
+    let connection_pool = create_configuration_pool(&configuration.database)?;
 
     let address = format!(
         "{}:{}",
@@ -22,5 +22,16 @@ async fn main() -> Result<(), std::io::Error> {
     );
 
     let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+    Ok(run(listener, connection_pool)?.await?)
+}
+
+fn create_configuration_pool(db_config: &DatabaseSettings) -> Result<Pool, Box<dyn std::error::Error>> {
+    let mut cfg = Config::new();
+    cfg.host = Some(db_config.host.clone());
+    cfg.port = Some(db_config.port.clone());
+    cfg.user = Some(db_config.username.clone());
+    cfg.password = Some(db_config.password.expose_secret().clone());
+    cfg.dbname = Some(db_config.database_name.clone());
+
+    cfg.create_pool(Some(Runtime::Tokio1), NoTls).map_err(|e| e.into())
 }
