73ef00fdbdd2f473644231584dd5293f3c27f21f
sproj <jonesalan404@gmail.com>
2025-05-30 14:02:17 +0100

    clippy + fmt

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use deadpool_postgres::{Config, Pool, Runtime};
 use secrecy::ExposeSecret;
-use tokio_postgres::NoTls;
 use std::net::TcpListener;
+use tokio_postgres::NoTls;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
@@ -25,13 +25,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(run(listener, connection_pool)?.await?)
 }
 
-fn create_configuration_pool(db_config: &DatabaseSettings) -> Result<Pool, Box<dyn std::error::Error>> {
+fn create_configuration_pool(
+    db_config: &DatabaseSettings,
+) -> Result<Pool, Box<dyn std::error::Error>> {
     let mut cfg = Config::new();
     cfg.host = Some(db_config.host.clone());
-    cfg.port = Some(db_config.port.clone());
+    cfg.port = Some(db_config.port);
     cfg.user = Some(db_config.username.clone());
     cfg.password = Some(db_config.password.expose_secret().clone());
     cfg.dbname = Some(db_config.database_name.clone());
 
-    cfg.create_pool(Some(Runtime::Tokio1), NoTls).map_err(|e| e.into())
+    cfg.create_pool(Some(Runtime::Tokio1), NoTls)
+        .map_err(|e| e.into())
 }
