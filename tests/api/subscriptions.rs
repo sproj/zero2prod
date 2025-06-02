3f91b7e0df5c464a5dc3fbcce0d944b014a94e78
sproj <jonesalan404@gmail.com>
2025-06-02 22:21:45 +0100

    test app setup should follow actual app setup factor `post_subscriptions` common request out of integration tests to `TestApp`

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -6,13 +6,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
     // Act
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
-    let response = client
-        .post(&format!("{}/subscriptions", &app.address))
-        .header("Content-Type", "application/x-www-form-urlencoded")
-        .body(body)
-        .send()
-        .await
-        .expect("Failed to execute request.");
+    let response = app.post_subscriptions(body.into());
 
     // Assert
     assert_eq!(200, response.status().as_u16());
@@ -42,13 +36,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 
     for (invalid_body, error_message) in test_cases {
         // Act
-        let response = client
-            .post(&format!("{}/subscriptions", &app.address))
-            .header("Content-Type", "application/x-www-form-urlencoded")
-            .body(invalid_body)
-            .send()
-            .await
-            .expect("Failed to execute request.");
+        let response = app.post_subscriptions(invalid_body.into());
 
         // Assert
         assert_eq!(
@@ -74,13 +62,7 @@ async fn subscribe_returns_400_when_fields_are_present_but_empty() {
 
     for (body, description) in test_cases {
         //act
-        let response = client
-            .post(&format!("{}/subscriptions", &app.address))
-            .header("Content-Type", "application/x-www-form-urlencoded")
-            .body(body)
-            .send()
-            .await
-            .expect("Failed to execute request");
+        let response = app.post_subscriptions(body.into());
         // assert
         assert_eq!(
             400,
