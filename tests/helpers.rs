2616b916acf623c33d4fedfab3035de0faa8e152
sproj <jonesalan404@gmail.com>
2025-06-02 19:37:08 +0100

    email client timeout to configurable value

--- tests/helpers.rs
+++ tests/helpers.rs
@@ -158,10 +158,13 @@ impl TestApp {
             .email_client
             .sender()
             .expect("Invalid sender email address.");
+
+        let timeout = configuration.email_client.timeout();
         let email_client = EmailClient::new(
             configuration.email_client.base_url,
             sender_email,
             configuration.email_client.authorization_token,
+            timeout,
         );
 
         // Setup application
