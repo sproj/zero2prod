4eafb364f0145b818c6b2b500d051e11c970ba52
sproj <jonesalan404@gmail.com>
2025-06-02 22:02:58 +0100

    factor server build stage out to startup

---
+++ tests/api/health_check.rs
@@ -0,0 +1,17 @@
+#[tokio::test]
+async fn health_check_works() {
+    // Arrange
+    let app = TestApp::spawn().await;
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get(&format!("{}/health_check", &app.address))
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(response.content_length(), Some(0));
+}
